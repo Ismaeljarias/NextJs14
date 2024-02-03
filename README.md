51a3e22a4c35aceddaf3c8d5dde38c5ed54e18ac
ismaelarias <ismaeljarias@gmail.com>
2024-02-03 12:47:36 -0600

    Next JS project start

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+### Just a simple project to test the Next.js 14 framework
 
 ## Getting Started
 
---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { createContext, useEffect, useState } from "react";
+
+type ThemeContextType = {
+  mode: string;
+  setMode: (mode: string) => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+
+  const handleThemeChange = () => {
+    if (mode === "dark") {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    } else {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
+
+  useEffect(
+    function useMode() {
+      handleThemeChange();
+    },
+    [mode],
+  );
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = React.useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+}
